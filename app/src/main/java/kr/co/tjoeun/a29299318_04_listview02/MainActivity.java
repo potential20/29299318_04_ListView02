877e2928da77fa70f015281c2cb29b5ff18050e4
potential20 <passion212@nate.com>
2020-03-18 17:13:29 +0900

    방 데이터 전달  1) Room 클래스 : implments Serializable  2) intent.putExtra("room", 방변수 통째로);  3) 받아줄때 (Room) getSerializableExtra

--- app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/MainActivity.java
@@ -3,6 +3,7 @@ package kr.co.tjoeun.a29299318_04_listview02;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,12 +39,14 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Log.i("리스트뷰 아이템 클릭",String.format("%d번 줄 클릭",position));
-
-//                "클릭된 방"의 주소를 Toast로 출력
-
+//                클릭 된 방의 정보를 목록에서 빼옴. position번째
                 Room clickedRoom = roomDatas.get(position);
-                Toast.makeText(mContext, clickedRoom.getAddress(), Toast.LENGTH_SHORT).show();
+
+//                방 상세 화면으로 이동.
+                Intent intent = new Intent(mContext,RoomDetailActivity.class);
+                intent.putExtra("room",clickedRoom);
+                startActivity(intent);
+
 
             }
         });
--- app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/RoomDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/RoomDetailActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import kr.co.tjoeun.a29299318_04_listview02.databinding.ActivityRoomDetailBinding;
+import kr.co.tjoeun.a29299318_04_listview02.datas.Room;
 
 public class RoomDetailActivity extends BaseActivity {
 
@@ -27,6 +28,9 @@ public class RoomDetailActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+//        첨부된 방 데이터를 받아서 화면에 출력.
+        Room room = (Room) getIntent().getSerializableExtra("room");
 
     }
 }
