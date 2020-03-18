986020472d3863847ca8a1ad08eca9e928a01afe
potential20 <passion212@nate.com>
2020-03-18 14:33:02 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a29299318_04_listview02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
