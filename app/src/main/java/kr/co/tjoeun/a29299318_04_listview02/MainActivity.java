b2fcdf28d9e50861275dbab8b74ff8b31c1bd389
potential20 <passion212@nate.com>
2020-03-18 14:44:16 +0900

    프로젝트 기본 세팅 완료

--- app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a29299318_04_listview02/MainActivity.java
@@ -1,14 +1,31 @@
 package kr.co.tjoeun.a29299318_04_listview02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import kr.co.tjoeun.a29299318_04_listview02.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
